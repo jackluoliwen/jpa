7df781d868a2e78604c4526666a9883424d23245
luoliwen <luoliwen@yztctech.com>
2017-04-18 22:11:09 +0800

    add

---
+++ JPA_DEMO1/src/main/java/com/sudojava/jpa/domain/Person.java
@@ -0,0 +1,87 @@
+package com.sudojava.jpa.domain;
+
+import javax.persistence.*;
+
+/**
+ * Created by luoliwen on 17/4/18.
+ */
+@Entity
+@Table(name = "t_person")
+public class Person {
+
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String address;
+
+    public Person() {
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        if (address != null ? !address.equals(person.address) : person.address != null) return false;
+        if (age != null ? !age.equals(person.age) : person.age != null) return false;
+        if (id != null ? !id.equals(person.id) : person.id != null) return false;
+        if (name != null ? !name.equals(person.name) : person.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (age != null ? age.hashCode() : 0);
+        result = 31 * result + (address != null ? address.hashCode() : 0);
+        return result;
+    }
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Person(Integer id, String name, Integer age, String address) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+}
---
+++ JPA_DEMO1/src/main/java/com/sudojava/jpa/utils/JPAManager.java
@@ -0,0 +1,96 @@
+package com.sudojava.jpa.utils;
+
+import com.sudojava.jpa.domain.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ * Created by luoliwen on 17/4/18.
+ */
+public class JPAManager<T extends Person> {
+
+    /**
+     * 本类实例
+     */
+    private static JPAManager manager;
+    /**
+     * 实体管理类的工厂对象
+     */
+    private EntityManagerFactory factory;
+    /**
+     * 实体管理类
+     */
+    private EntityManager entityManager;
+    /**
+     * 事务处理
+     */
+    private EntityTransaction transaction;
+
+    private JPAManager() {
+        factory = Persistence.createEntityManagerFactory("jpa");
+        entityManager = factory.createEntityManager();
+    }
+
+    /**
+     * 采用单例模式提供JPA访问类的实例
+     * @return
+     */
+    public static synchronized JPAManager getInstance() {
+        if (manager == null) {
+            manager = new JPAManager();
+        }
+        return manager;
+    }
+
+    /**
+     * 关闭实体类和工厂类
+     */
+    public void close(){
+        if (entityManager!=null){
+            entityManager.close();
+        }
+        if (factory!=null){
+            factory.close();
+        }
+    }
+
+    /**
+     * 保存 Java对象
+     * @param t
+     * @return
+     * @throws RuntimeException
+     */
+    public T saveObject(T t) throws RuntimeException {
+
+        try {
+            transaction = entityManager.getTransaction();
+            transaction.begin();
+            t = entityManager.contains(t) ? t : entityManager.merge(t);
+            transaction.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("保存失败！");
+        } finally {
+            close();
+        }
+        return t;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        Person person = new Person();
+        person.setName("X");
+        person.setAddress("XX");
+        person.setAge(23);
+
+        JPAManager.getInstance().saveObject(person);
+    }
+}
